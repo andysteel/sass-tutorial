7a4d31b1703ba8652d1cd3c5d19c2e3ee2e463a3
Anderson <andersoninfonet@gmail.com>
2021-09-19 16:39:11 -0300

    🚩 custom css lib complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 const buildStyles = () => {
   return src('andysteel/**/*.scss')
-    .pipe(sass())
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'));
 }
 
 const watchTask = () => {
-  watch(['andysteel/**/*.scss'], buildStyles);
+  watch(['sass/**/*.scss', '*.html'], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask)
